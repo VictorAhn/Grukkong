107e8c670cda0b30a7c125f9961d7e533bcb2e80
VictorAhn <my__precious@naver.com>
2018-07-20 14:58:17 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,12 @@
-
 <?php
-//회사 정보 (사원 리스트 등)
 error_reporting(E_ALL); 
 ini_set('display_errors',1); 
 
-//db 열기위한 값 불러옴
-//$pin=isset($_POST['pin']) ? $_POST['pin'] : '';
 $rt_start=isset($_POST['rt_start']) ? $_POST['rt_start'] : '';
 $rt_end=isset($_POST['rt_end']) ? $_POST['rt_end'] : '';
 $latitude=isset($_POST['latitude']) ? $_POST['latitude'] : '';
 $longitude=isset($_POST['longitude']) ? $_POST['longitude'] : '';
 
-//if ($pin =="") echo "PIN번호를 입력하세요.";
-//else
-{
-    //$link = mysqli_connect("localhost", "root","", "com_".$pin);
     $link = mysqli_connect("localhost", "root","", "com_1");
     if (!$link)
     {
@@ -48,36 +40,4 @@ $longitude=isset($_POST['longitude']) ? $_POST['longitude'] : '';
         $up_date = strtotime($row->reg_date);
         mysqli_query($link,"UPDATE company_info SET rest_time_start='j',rest_time_end='j', latitude='$latitude', longitude='$longitude', reg_date='$now_time' WHERE reg_date='$up_date'");
     }
-
-
-
-}
-
 mysqli_close($link);
-
-
-?>
-
-<?php
-
-$android = strpos($_SERVER['HTTP_USER_AGENT'], "Android");
-$iPhone = strpos($_SERVER['HTTP_USER_AGENT'], "iPhone");
-
-if (!$android && !$iPhone){
-    header('Content-Type: text/html; charset=utf-8');
-?>
-
-<html5>
-   <body>
-      <form action="<?php $_PHP_SELF ?>" method="POST">
-         rest1: <input type = "text" name = "rt_start" />
-         rest2: <input type = "text" name = "rt_end" />
-         lati: <input type = "text" name = "latitude" />
-         longi: <input type = "text" name = "longitude"/>
-         <input type = "submit" value = "제출"/>
-      </form>
-   </body>
-</html5>
-<?php
-}
-?>
