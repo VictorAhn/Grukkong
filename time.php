b9ef3ee988d644f5144a97be63db7bf6a546331f
VictorAhn <my__precious@naver.com>
2018-07-20 15:09:33 +0900

    Update time.php

--- time.php
+++ time.php
@@ -1,8 +1,3 @@
 <?php
-date_default_timezone_set("Asia/Seoul");
-$rev_time = date("Y-m-d", time())." 13:00:00";
-echo $rev_time;
-echo '</br>';
-$timea = date("Y-m-d H:i:s", time());
-echo $timea;
+echo "Test";
 ?>
